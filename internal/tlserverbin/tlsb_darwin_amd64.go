35a162731b236f84ff3b512f8966cfe0065054d6
Harry Harpham <harry@getlantern.org>
2020-05-20 20:47:45 -0600

    Update (still unsigned) embedded tlserver binary

--- internal/tlserverbin/tlsb_darwin_amd64.go
+++ internal/tlserverbin/tlsb_darwin_amd64.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0eedb78dc441e90a004a2c572a9cb827f00c784df03f90000d38a80e803bcfb0
-size 29162429
+oid sha256:7368a182c73cbacbeb9c2053b39d30ef3d364c8ef4c0537cc68bee1107619af7
+size 28599653

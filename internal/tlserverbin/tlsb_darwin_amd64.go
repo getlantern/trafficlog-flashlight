668c8e3b5e687a4650db3897ae0df5609184ddb1
Ox Cart <ox.to.a.cart@gmail.com>
2020-09-23 10:37:31 -0500

    Signed binaries

--- internal/tlserverbin/tlsb_darwin_amd64.go
+++ internal/tlserverbin/tlsb_darwin_amd64.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:48ebe3ef07bcada92f196f9457dc979e2dc71abe982053459039ad596c6acd34
-size 34679202
+oid sha256:7418b1cac5dc4455c77a3062e0781ef584790f8bf99ab39688c59b18862f9049
+size 35410393
--- internal/tlserverbin/tlsb_debug_darwin_amd64.go
+++ internal/tlserverbin/tlsb_debug_darwin_amd64.go
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5027f8fed4376b6dbbf5918abff5142fef699ed569c619d686100d95e075fc76
-size 34690317
+oid sha256:6e7eb0e783390c34cab8f295aec093d832e6a89b264766ef9ff970a1a9813e41
+size 34688792
